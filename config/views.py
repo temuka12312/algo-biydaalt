4098fc6f4b5674d59ad18b1fc8c074c135aad8d5
temuulen.m <temuulen.m@databank.mn>
2024-11-14 09:16:47 +0800

    template nemsen

--- config/views.py
+++ config/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+import os
+import json
+import logging
+
+from django.http import JsonResponse
+
+from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+class First(TemplateView):
+    template_name = "index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,9 @@ from django.urls import include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from config.views import First
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", First.as_view(), name='home'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
