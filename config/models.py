574ab71d65e946af153a56f7536e63a26edee243
unknown <sanki0715@gmail.com>
2024-11-14 10:02:09 -0800

    sex

--- config/models.py
+++ config/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class test():
+    name = models.CharField(verbose_name="test", max_length=15)
+    
+    class Meta:
+        verbose_name = "test"
+        verbose_name_plural = "test"
+        
+    def __str__(self) -> str:
+        return self.name
--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cugx+&iw0%&8@4ln*vokme_l@ox(tb=igfer$l1m11eubxp*w6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
