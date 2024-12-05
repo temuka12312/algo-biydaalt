92ed384da8c16a39ef5abcb90addd9041918ad17
temuulen.m <temuulen.m@databank.mn>
2024-12-05 12:28:10 +0800

    direc yanzalsan

--- config/views.py
+++ config/views.py
@@ -6,30 +6,48 @@ import os
 
 logger = logging.getLogger(__name__)
 
+# SpellChecker тохируулах
 spell = SpellChecker(language=None)
 
-# Load the Mongolian dictionary files (mn.dic and mn.aff)
-dic_path = os.path.join('mn.dic')
-aff_path = os.path.join('mn.aff')
+# Монгол хэлний толь бичгийн файлуудыг зааж өгөх
+mn_dic_path = os.path.join('mn.dic')
+mn_aff_path = os.path.join('mn.aff')
 
 try:
-    spell.word_frequency.load_text_file(dic_path)
-    spell.word_frequency.load_text_file(aff_path)
+    # mn.dic файлыг SpellChecker-д нэмэх
+    spell.word_frequency.load_text_file(mn_dic_path)
+    logger.info("mn.dic амжилттай ачааллаа.")
+
+    # mn.aff файлыг SpellChecker-д дүрмийн дагуу нэмэх
+    spell.word_frequency.load_text_file(mn_aff_path)
+    logger.info("mn.aff амжилттай ачааллаа.")
 except FileNotFoundError as e:
-    logger.error(f"Mongolian dictionary file not found: {e}")
+    logger.error(f"Монгол хэлний толь бичгийн файл олдсонгүй: {e}")
 except Exception as e:
-    logger.error(f"An error occurred while loading the word list: {e}")
+    logger.error(f"Толь бичгийг ачаалахад алдаа гарлаа: {e}")
 
 @csrf_exempt
 def spell_check_view(request):
     suggestions = {}
     text = ""
-    
+
     if request.method == 'POST':
         text = request.POST.get('text', '') 
-        words = text.split()  
-        misspelled_words = spell.unknown(words)  
+        words = text.split()  # Текстийг үгсэд хуваах
 
-        suggestions = {word: spell.correction(word) for word in misspelled_words}
+        misspelled_words = spell.unknown(words)
 
-    return render(request, 'index.html', {'text': text, 'suggestions': suggestions})
+        for word in misspelled_words:
+            if word in spell:
+                suggestions[word] = word  # Үг өөрөө зөв гэж тооцох
+            else:
+                suggestions[word] = spell.correction(word)
+
+    # Front-end-д буцаах
+    return render(request, 'index.html', {
+        'text': text,
+        'suggestions': suggestions
+    })
+
+def about(request):
+    return render(request, 'about.html')
--- core/urls.py
+++ core/urls.py
@@ -19,8 +19,10 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from config.views import spell_check_view
+from config.views import about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', spell_check_view, name='spell_check'),
+    path('about', about, name='about'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
